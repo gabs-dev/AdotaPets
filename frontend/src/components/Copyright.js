c8544080a170d854dce0f2979b2281131c33ce29
gabs-dev <gabrielfelipe1458@gmail.com>
2022-09-22 13:59:43 -0300

    Implementação parcial da tela de login e inclusão do pacote do material ui.

---
+++ frontend/src/components/Copyright.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+
+export default function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="#">
+        AdotaPets
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
