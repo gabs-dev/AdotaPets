a55f1fa7aecb8cd7185f2375612613a9cee546cb
gabs-dev <gabrielfelipe1458@gmail.com>
2022-10-18 16:47:39 -0300

    Alterações na tela de login e comunicação com a api para realizar login.

--- frontend/src/components/Cadastro/FormCadastro.js
+++ frontend/src/components/Cadastro/FormCadastro.js
@@ -5,7 +5,6 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 /*import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';*/
-import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
@@ -40,9 +39,6 @@ function FormCadastro() {
                     md={7}
                     sx={{
                         backgroundImage: `url(${CatBackground})`,
-                        backgroundRepeat: 'no-repeat',
-                        backgroundColor: (t) =>
-                            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
                         backgroundSize: 'cover',
                         backgroundPosition: 'center',
                     }}
@@ -95,7 +91,7 @@ function FormCadastro() {
                                 autoComplete="current-password"
                             />
                             <TextField
-                                margin="small"
+                                margin="normal"
                                 required
                                 fullWidth
                                 name="password"
